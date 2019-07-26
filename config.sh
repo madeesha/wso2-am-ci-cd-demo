d779ac032058edb485faa74182440c364d560cf3
madeesha <madeeshafernando@gmail.com>
2019-07-26 10:01:33 +0530

    change prod properties

--- config.sh
+++ config.sh
@@ -22,12 +22,12 @@ apimcli login test -u $USERNAME -p $PASSWORD -k
 
 echo 'setting up prod environment'
 apimcli add-env -n prod \
-                    --registration https://localhost:9445/client-registration/v0.14/register \
+                    --registration https://localhost:9443/client-registration/v0.14/register \
                     --apim https://localhost:9443 \
-                    --token https://localhost:8245/token \
-                    --import-export https://localhost:9445/api-import-export-3.0.0-SNAPSHOT \
-                    --admin https://localhost:9445/api/am/admin/v0.14 \
-                    --api_list https://localhost:9445/api/am/publisher/v0.14/apis \
-                    --app_list https://localhost:9445/api/am/store/v0.14/applications
+                    --token https://localhost:8243/token \
+                    --import-export https://localhost:9443/api-import-export-3.0.0-SNAPSHOT \
+                    --admin https://localhost:9443/api/am/admin/v0.14 \
+                    --api_list https://localhost:9443/api/am/publisher/v0.14/apis \
+                    --app_list https://localhost:9443/api/am/store/v0.14/applications
 echo 'logging into prod'
 apimcli login prod -u $USERNAME -p $PASSWORD -k
