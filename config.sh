6d4ba313868cb4efe605980ae34c2c335a448cab
madeesha <madeeshafernando@gmail.com>
2019-07-28 09:22:58 +0530

    add staging environment

--- config.sh
+++ config.sh
@@ -20,6 +20,18 @@ apimcli add-env -n test \
 echo 'logging into test'
 apimcli login test -u $USERNAME -p $PASSWORD -k
 
+echo 'setting up staging environment'
+apimcli add-env -n staging \
+                      --registration https://localhost:9443/client-registration/v0.14/register \
+                      --apim https://localhost:9443/publisher \
+                      --token https://localhost:8243/token \
+                      --import-export https://localhost:9443/api-import-export-2.6.0-v10 \
+                      --admin https://localhost:9443/api/am/admin/v0.14 \
+                      --api_list https://localhost:9443/api/am/publisher/v0.14/apis \
+                      --app_list https://localhost:9443/api/am/store/v0.14/applications
+echo 'logging into staging'
+apimcli login test -u $USERNAME -p $PASSWORD -k
+
 echo 'setting up prod environment'
 apimcli add-env -n prod \
                       --registration https://localhost:9443/client-registration/v0.14/register \
