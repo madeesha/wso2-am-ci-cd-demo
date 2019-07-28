dcf4a5ac30f928e388304cc8371521341af042c9
madeesha <madeeshafernando@gmail.com>
2019-07-28 09:11:25 +0530

    duplicate same environment to test as prod

--- config.sh
+++ config.sh
@@ -10,13 +10,13 @@ rm -rf $HOME/.wso2apimcli
 
 echo 'setting up test environment'
 apimcli add-env -n test \
-                      --registration https://localhost:9444/client-registration/v0.14/register \
-                      --apim https://localhost:9444/publisher \
-                      --token https://localhost:8244/token \
-                      --import-export https://localhost:9444/api-import-export-2.6.0-v10 \
-                      --admin https://localhost:9444/api/am/admin/v0.14 \
-                      --api_list https://localhost:9444/api/am/publisher/v0.14/apis \
-                      --app_list https://localhost:9444/api/am/store/v0.14/applications
+                      --registration https://localhost:9443/client-registration/v0.14/register \
+                      --apim https://localhost:9443/publisher \
+                      --token https://localhost:8243/token \
+                      --import-export https://localhost:9443/api-import-export-2.6.0-v10 \
+                      --admin https://localhost:9443/api/am/admin/v0.14 \
+                      --api_list https://localhost:9443/api/am/publisher/v0.14/apis \
+                      --app_list https://localhost:9443/api/am/store/v0.14/applications
 echo 'logging into test'
 apimcli login test -u $USERNAME -p $PASSWORD -k
 
